a0625652805ceb7c993c07ed9e8e55b8b6bc6f8d
yortz <f.m.cortini@gmail.com>
2019-01-18 11:57:18 +0000

    Fixes Rspec deprecation warnings.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,10 +10,9 @@ OmniAuth.config.logger = Logger.new('/dev/null')
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-  
+
   include Rack::Test::Methods
 
   # Run specs in random order to surface order dependencies. If you find an
